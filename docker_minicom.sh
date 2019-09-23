780fb46027997adac6a6e0464e08346bbd8b08bf
Sladuca <sladuca777@gmail.com>
2019-09-23 18:27:39 -0400

    tell minicom which device to open

--- docker_minicom.sh
+++ docker_minicom.sh
@@ -3,6 +3,5 @@
 # runs a container with minicom listening on device
 # ARGS: 
 # 1 - device ID to listen to, i.e. ttyACM0 as in /dev/ttyACM0
-# 2 (optional) - baud rate. Defaults to 9600
 
-docker run -t -i --device/dev/$1
+docker run -t -i --device/dev/$1 /dev/$1
